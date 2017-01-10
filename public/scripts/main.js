b66e146fa7e6c69aeee44c31ee1e7f225b7fc5f6
Jennifer Ali <Jennifer Ali>
2017-01-10 14:10:09 +0000

    update

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,79 @@
 $(document).ready(function(){
 
-    $('#calendar').fullCalendar();
+/*==========================
+
+            MODEL
+
+=============================*/
+
+//------- ARRAYS ----------//
+
+
+// ------ OBJECTS -----------------//
+
+
+//------- FUNCTION CONSTRUCTORS ----------//
+
+
+//------- ARRAY UPDATERS VIA FUNCTION CONSTRUCTORS -----//
+
+/*===========================
+
+            VIEW
+
+=============================*/
+
+//------- SELECTORS ----------//
+var $galleryPhoto = $('.gallery-photo img');
+var $lightboxHome = $('#lightboxHome');
+var $lightboxCancelBtn = $('.lightbox__cancel-btn');
+var $lightboxImg = $('.lightbox-img img');
+
+//--------VARIABLES ----------//
+var open = false;
+
+//------ TEMPLATES ---------//
+
+
+//------- EVENTS ----------//
+$galleryPhoto.on({
+    'click': function() {
+        name = $(this).attr('src');
+        open = true;
+        lightboxController(open, name);
+    }
+});
+
+$lightboxCancelBtn.on({
+    'click' : function() {
+        open = false;
+        lightboxController(open);
+    }
+});
+
+/*===========================
+
+        CONTROLLER
+
+=============================*/
+
+//------- FUNCTIONS ----------//
+
+//lightbox CONTROLLER
+function lightboxController(){
+    if(open){
+        $lightboxHome.show();
+    } else {
+        $lightboxHome.hide();
+    }
+
+    $lightboxImg.attr('src' , name);
+}
+
+
+//------- FUNCTION CALLS ----------//
+$('#calendar').fullCalendar();
+lightboxController(open);
+
+//console.log($('.gallery-photo'));
 });
