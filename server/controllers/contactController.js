abf46946df6e83031332774588edd61bf93d76db
Jennifer Ali <Jennifer Ali>
2017-01-12 11:50:22 +0000

    update

--- server/controllers/contactController.js
+++ server/controllers/contactController.js
@@ -1,7 +1,33 @@
 /** CONTACT CONTROLLER */
+var formResponse = require('../models/contactModel');
+var errorHelper = require('../util/errorHelper');
 
-const contactController = function(req, res) {
+
+exports.renderPage = function(req, res) {
     res.render('contact');
-}
+};
 
-module.exports = contactController;
+
+exports.sendToDb = function(req, res) {
+
+    var entry = new formResponse({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        message: req.body.message
+    });
+
+    entry.save(function(err) {
+        if(err) {
+            
+            var validation = errorHelper(err);
+            console.log(validation);
+            res.render('contact', validation);
+
+        } else {
+            res.render('contact',{success: true});
+        }
+    });
+
+};
+
